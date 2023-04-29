e0112a73bbf14b0eb1120add77437aaaa01a8997
Shoaib Shaikh <shoaib.shaikh@momentum.co.za>
2023-04-29 18:57:37 +0530

    Used constructor injection

--- src/main/java/ml/hadiya/services/SpeakerServiceImpl.java
+++ src/main/java/ml/hadiya/services/SpeakerServiceImpl.java
@@ -9,11 +9,17 @@ import java.util.List;
 public class SpeakerServiceImpl implements SpeakerService {
 
     private SpeakerRepository speakerRepository;
+
+    // Using constructor injection
+    public SpeakerServiceImpl(SpeakerRepository repository) {
+        this.speakerRepository = repository;
+    }
     @Override
     public List<Speaker> listSpeakers() {
         return speakerRepository.findAll();
     }
 
+    // Using setter injection
     public void setSpeakerRepository(SpeakerRepository repository) {
         this.speakerRepository = repository;
     }
